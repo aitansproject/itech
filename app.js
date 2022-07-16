09b36d55e6801c8ef1349eba899950bbeba1f96e
end4project <aytennadirli1@mail.ru>
2022-07-16 10:03:30 +0000

    comfortable

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 "use strict";
 $(document).ready(function () {
 
+    $('.btn').click(function (e) {
+        e.preventDefault();
+        if ($.trim($('input').val()) == '') {
+            $('input').val();
+        } else {
+            let k = v => {
+                for (let i = 0; i < v; i++) { $('.minibox').append(`<h3>${v}</h3>`) };
+            }
+            let z = e => Number(e) ? k(e) : $('.minibox').append(`<h3>${e}</h3>`);
+            z($('input').val());
+        }
+        $('input').val('')
+    })
+
 });
